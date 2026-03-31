f1e6f1b98f50a10bda65831627a599dd477a6c63
Lipika_21 <baglipika.01@gmail.com>
2026-03-31 16:41:37 +0530

    HisabKitab Project

---
+++ config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DB_NAME = os.environ.get("DB_NAME", "hisabkitab")
+if not MONGO_URI:
+    raise ValueError("MONGO_URI is not set")
